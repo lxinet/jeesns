52a53c9d5b0310ea60c5a6cec5540a225257648e
zchuanzhao <zchuanzhao@163.com>
2017-04-12 16:51:17 +0800

    修复多次运行积分规则SQL导致ID不一致问题

--- src/main/webapp/database/update_1.0.1.sql
+++ src/main/webapp/database/update_1.0.1.sql
@@ -24,22 +24,22 @@ CREATE TABLE `tbl_score_detail` (
   `score_rule_id` INT(11) DEFAULT '0' COMMENT '积分规则ID',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?6e47fd0326365727a8e2ed62d5935c88";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>
 
-INSERT INTO tbl_score_rule(create_time,update_time,name,score,remark,type,status) VALUES
-  (now(),now(),'注册奖励',100,'注册奖励','one',1),
-  (now(),now(),'邮箱认证',1,'邮箱认证奖励积分，只有一次','one',1),
-  (now(),now(),'每天登陆奖励',1,'每天登陆奖励积分，一天仅限一次','day',1),
-  (now(),now(),'文章投稿',1,'文章投稿奖励积分，如需审核，审核之后奖励','unlimite',1),
-  (now(),now(),'文章评论',1,'评论文章奖励积分','unlimite',1),
-  (now(),now(),'文章收到喜欢',1,'文章收到喜欢，作者奖励积分','unlimite',1),
-  (now(),now(),'发布微博',1,'发布微博奖励积分','unlimite',1),
-  (now(),now(),'评论微博',1,'评论微博奖励积分','unlimite',1),
-  (now(),now(),'微博收到点赞',1,'微博收到点赞，作者奖励积分','unlimite',1),
-  (now(),now(),'申请群组',-10,'申请群组扣除/奖励积分，如需要扣除积分，请填写负数','unlimite',1),
-  (now(),now(),'群组发帖',1,'群组发帖奖励积分，如需审核，审核之后奖励','unlimite',1),
-  (now(),now(),'群组帖子评论',1,'群组帖子评论奖励积分','unlimite',1),
-  (now(),now(),'群组帖子收到喜欢',1,'群组帖子收到喜欢奖励积分','unlimite',1);
+
+INSERT INTO tbl_score_rule(id,create_time,update_time,name,score,remark,type,status) VALUES
+  (1,now(),now(),'注册奖励',100,'注册奖励','one',1),
+  (2,now(),now(),'邮箱认证',1,'邮箱认证奖励积分，只有一次','one',1),
+  (3,now(),now(),'每天登陆奖励',1,'每天登陆奖励积分，一天仅限一次','day',1),
+  (4,now(),now(),'文章投稿',1,'文章投稿奖励积分，如需审核，审核之后奖励','unlimite',1),
+  (5,now(),now(),'文章评论',1,'评论文章奖励积分','unlimite',1),
+  (6,now(),now(),'文章收到喜欢',1,'文章收到喜欢，作者奖励积分','unlimite',1),
+  (7,now(),now(),'发布微博',1,'发布微博奖励积分','unlimite',1),
+  (8,now(),now(),'评论微博',1,'评论微博奖励积分','unlimite',1),
+  (9,now(),now(),'微博收到点赞',1,'微博收到点赞，作者奖励积分','unlimite',1),
+  (10,now(),now(),'申请群组',-10,'申请群组扣除/奖励积分，如需要扣除积分，请填写负数','unlimite',1),
+  (11,now(),now(),'群组发帖',1,'群组发帖奖励积分，如需审核，审核之后奖励','unlimite',1),
+  (12,now(),now(),'群组帖子评论',1,'群组帖子评论奖励积分','unlimite',1),
+  (13,now(),now(),'群组帖子收到喜欢',1,'群组帖子收到喜欢奖励积分','unlimite',1);
 
 ##修改jvalue字段长度为500
 ALTER TABLE tbl_config MODIFY jvalue varchar(500) DEFAULT '';
