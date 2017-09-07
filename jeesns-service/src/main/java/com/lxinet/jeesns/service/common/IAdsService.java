7268719870fff682ff676fc00782e714a4c391a6
Mini_Moon <972242736@qq.com>
2017-09-07 17:06:56 +0800

    广告模块

---
+++ jeesns-service/src/main/java/com/lxinet/jeesns/service/common/IAdsService.java
@@ -0,0 +1,30 @@
+package com.lxinet.jeesns.service.common;
+
+import com.lxinet.jeesns.core.dto.ResponseModel;
+import com.lxinet.jeesns.core.model.Page;
+import com.lxinet.jeesns.model.common.Ads;
+
+/**
+ * Created by MMF on 2017-09-07.
+ */
+public interface IAdsService {
+    /**
+     * 保存广告信息
+     * @param ads
+     * @return
+     */
+    ResponseModel save(Ads ads);
+    /**
+     * 分页查询广告信息
+     * @param page
+     * @return
+     */
+    ResponseModel listByPage(Page page);
+
+    ResponseModel update(Ads ads);
+
+    ResponseModel delete(Integer id);
+
+    Ads findByID(Integer id);
+
+}
