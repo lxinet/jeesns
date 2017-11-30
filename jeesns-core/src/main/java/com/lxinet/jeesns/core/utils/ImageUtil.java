9c50e6c9490070916b57e1f4442061c8b951ad41
zchuanzhao <zchuanzhao@163.com>
2017-11-30 13:15:07 +0800

    修改上传图片生成缩略图算法

--- jeesns-core/src/main/java/com/lxinet/jeesns/core/utils/ImageUtil.java
+++ jeesns-core/src/main/java/com/lxinet/jeesns/core/utils/ImageUtil.java
@@ -18,9 +18,10 @@ import java.util.Iterator;
 public class ImageUtil {
     public static final String THUMB_DEFAULT_PREVFIX = "thumb_";
     public static final String SMALL_DEFAULT_PREVFIX = "small_";
-    private static final Boolean DEFAULT_FORCE = true;
     private static final int DEFAULT_WIDTH = 260;
-    private static final int DEFAULT_HEIGHT = 260;
+    private static final int DEFAULT_HEIGHT = 160;
+    private static final int THUMB_DEFAULT_WIDTH = 160;
+    private static final int THUMB_DEFAULT_HEIGHT = 160;
     private File targetFile;
 
     public String dealImage(File imgFile){
@@ -30,19 +31,16 @@ public class ImageUtil {
         return cutImage(targetFile);
     }
 
-
-
     /**
      * 根据图片路径生成缩略图
      * @param imgFile 原图片
-     * @param w       缩略图宽
-     * @param h       缩略图高
      * @param prevfix 生成缩略图的前缀
-     * @param force   是否强制按照宽高生成缩略图(如果为false，则生成最佳比例缩略图)
      */
-    private String thumbnailImage(File imgFile, int w, int h, String prevfix, boolean force) {
+    private String thumbnailImage(File imgFile, String prevfix) {
         String thumbPath = "";
         String fileName = "";
+        int w;
+        int h;
         if (imgFile.exists()) {
             try {
                 // ImageIO 支持的图片类型 : [BMP, bmp, jpg, JPG, wbmp, jpeg, png, PNG, JPEG, WBMP, GIF, gif]
@@ -57,23 +55,14 @@ public class ImageUtil {
                     return thumbPath;
                 }
                 Image img = ImageIO.read(imgFile);
-                if (!force) {
-                    // 根据原图与要求的缩略图比例，找到最合适的缩略图比例
-                    int width = img.getWidth(null);
-                    int height = img.getHeight(null);
-                    if ((width * 1.0) / w < (height * 1.0) / h) {
-                        if (width > w) {
-                            h = Integer.parseInt(new java.text.DecimalFormat("0").format(height * w / (width * 1.0)));
-                        }else {
-                            w = width;
-                        }
-                    } else {
-                        if (height > h) {
-                            w = Integer.parseInt(new java.text.DecimalFormat("0").format(width * h / (height * 1.0)));
-                        }else {
-                            h = height;
-                        }
-                    }
+                int width = img.getWidth(null);
+                int height = img.getHeight(null);
+                if (width > height){
+                    h = DEFAULT_HEIGHT;
+                    w = (int) ((width * h * 1.0) / (height * 1.0));
+                }else {
+                    w = DEFAULT_WIDTH;
+                    h = (int) ((height * w * 1.0) / (width * 1.0));
                 }
                 BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
                 Graphics g = bi.getGraphics();
@@ -92,31 +81,25 @@ public class ImageUtil {
         return fileName;
     }
 
-    private String thumbnailImage(File imgFile, int w, int h, boolean force) {
-        return thumbnailImage(imgFile, w, h, SMALL_DEFAULT_PREVFIX, force);
-    }
-
     private String thumbnailImage(File imgFile) {
-        return thumbnailImage(imgFile, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_FORCE);
+        return thumbnailImage(imgFile, SMALL_DEFAULT_PREVFIX);
     }
 
 
     /**
      * 裁剪图片
      * @param imgFile
-     * @param w
-     * @param h
      * @return
      */
-    private String cutImage(File imgFile, int w, int h) {
-        FileInputStream is = null;
-        ImageInputStream iis = null;
+    private String cutImage(File imgFile) {
         String fileName = "";
-        String suffix = "";
         String thumbPath = "";
+        int x;
+        int y;
         try {
-
+            // ImageIO 支持的图片类型 : [BMP, bmp, jpg, JPG, wbmp, jpeg, png, PNG, JPEG, WBMP, GIF, gif]
             String types = Arrays.toString(ImageIO.getReaderFormatNames());
+            String suffix = null;
             // 获取图片后缀
             if (imgFile.getName().indexOf(".") > -1) {
                 suffix = imgFile.getName().substring(imgFile.getName().lastIndexOf(".") + 1);
@@ -125,115 +108,37 @@ public class ImageUtil {
             if (suffix == null || types.toLowerCase().indexOf(suffix.toLowerCase()) < 0) {
                 return thumbPath;
             }
-            // 读取图片文件
-            is = new FileInputStream(imgFile);
-
             Image img = ImageIO.read(imgFile);
             int width = img.getWidth(null);
             int height = img.getHeight(null);
-            int x;
-            int y;
-            if(width > w){
-                x = (width - w) / 2;
+            if (width > THUMB_DEFAULT_WIDTH){
+                x = (width - THUMB_DEFAULT_WIDTH) / 2;
             }else {
-                x = 0;
-                w = width;
+                x = THUMB_DEFAULT_WIDTH;
             }
-            if(height > h){
-                y = (height - h) / 2;
+            if (height > THUMB_DEFAULT_HEIGHT){
+                y = (height - THUMB_DEFAULT_HEIGHT) / 2;
             }else {
-                y = 0;
-                h = height;
+                y = THUMB_DEFAULT_HEIGHT;
             }
-            /**
-             *
-             * 返回包含所有当前已注册 ImageReader 的 Iterator，这些 ImageReader
-             *
-             * 声称能够解码指定格式。 参数：formatName - 包含非正式格式名称 .
-             *
-             * (例如 "jpeg" 或 "tiff")等 。
-             */
-            Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName(suffix);
-
-            ImageReader reader = it.next();
-
-            // 获取图片流
-            iis = ImageIO.createImageInputStream(is);
-
-            /**
-             *
-             * <p>
-             * iis:读取源。true:只向前搜索
-             * </p>
-             * .将它标记为 ‘只向前搜索’。
-             *
-             * 此设置意味着包含在输入源中的图像将只按顺序读取，可能允许 reader
-             *
-             * 避免缓存包含与以前已经读取的图像关联的数据的那些输入部分。
-             */
-            reader.setInput(iis, true);
-
-            /**
-             *
-             * <p>
-             * 描述如何对流进行解码的类
-             * <p>
-             * .用于指定如何在输入时从 Java Image I/O
-             *
-             * 框架的上下文中的流转换一幅图像或一组图像。用于特定图像格式的插件
-             *
-             * 将从其 ImageReader 实现的 getDefaultReadParam 方法中返回
-             *
-             * ImageReadParam 的实例。
-             */
-            ImageReadParam param = reader.getDefaultReadParam();
-
-            /**
-             *
-             * 图片裁剪区域。Rectangle 指定了坐标空间中的一个区域，通过 Rectangle 对象
-             *
-             * 的左上顶点的坐标(x，y)、宽度和高度可以定义这个区域。
-             */
-            Rectangle rect = new Rectangle(x, y, w, h);
-
-            // 提供一个 BufferedImage，将其用作解码像素数据的目标。
-            param.setSourceRegion(rect);
-
-            /**
-             *
-             * 使用所提供的 ImageReadParam 读取通过索引 imageIndex 指定的对象，并将
-             *
-             * 它作为一个完整的 BufferedImage 返回。
-             */
-            BufferedImage bi = reader.read(0,param);
-
+            BufferedImage bi = new BufferedImage(THUMB_DEFAULT_WIDTH, THUMB_DEFAULT_HEIGHT, BufferedImage.TYPE_INT_RGB);
+            Graphics g = bi.getGraphics();
+            g.drawImage(img, x, y, THUMB_DEFAULT_WIDTH, THUMB_DEFAULT_HEIGHT, Color.LIGHT_GRAY, null);
+            g.dispose();
             // 保存新图片
-            fileName = THUMB_DEFAULT_PREVFIX + imgFile.getName();
+            String name = imgFile.getName();
+            name = name.replaceAll(SMALL_DEFAULT_PREVFIX,"");
+            fileName = THUMB_DEFAULT_PREVFIX + name;
             String p = imgFile.getPath();
+            // 将图片保存在原目录并加上前缀
             thumbPath = p.substring(0, p.lastIndexOf(File.separator)) + File.separator + fileName;
-            ImageIO.write(bi, suffix, new File(thumbPath));
+            targetFile = new File(thumbPath);
+            ImageIO.write(bi, suffix, targetFile);
         } catch (Exception e){
             e.printStackTrace();
         } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (iis != null) {
-                try {
-                    iis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+
         }
         return fileName;
-    }
-
-    private String cutImage(File imgFile) {
-        return cutImage(imgFile, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 }
