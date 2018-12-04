37e7570d8e206d4bde7ffbf55a0b84bd36ae27b3
zchuanzhao <zchuanzhao@163.com>
2018-12-04 17:23:27 +0800

    优化富文本编辑器

--- jeesns-web/src/main/webapp/res/plugins/ckeditor/config.js
+++ jeesns-web/src/main/webapp/res/plugins/ckeditor/config.js
@@ -12,21 +12,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' }
-	];
+    config.toolbar = [
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image', 'Flash', 'Table' ] },
+        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+        { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', '-', 'Blockquote', 'CodeSnippet' ] },
+        { name: 'styles', items: [ 'Format','FontSize'] },
+        { name: 'tools', items: [ 'Source', 'Maximize' ] }
+    ];
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
@@ -37,7 +31,48 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+    config.uiColor = '#ffffff';
+    config.resize_dir = 'vertical';
+    config.defaultLanguage = 'zh-cn';
+    config.skin = 'moono-lisa';
+    //添加插件，多个插件用逗号隔开
+    config.extraPlugins = 'codesnippet,colorbutton,font';
+    config.codeSnippet_theme = 'zenburn';
+    config.codeSnippet_languages = {
+        apache       : 'Apache',
+        bash         : 'Bash',
+        cs           : 'C#',
+        cpp          : 'C++',
+        css          : 'CSS',
+        coffeescript : 'CoffeeScript',
+        delphi       : 'Delphi',
+        diff         : 'Diff',
+        go           : 'Go',
+        groovy       : 'Groovy',
+        html         : 'HTML',
+        http         : 'HTTP',
+        ini          : 'Ini',
+        json         : 'JSON',
+        java         : 'Java',
+        javascript   : 'JavaScript',
+        lua          : 'lua',
+        makefile     : 'Makefile',
+        markdown     : 'Markdown',
+        nginx        : 'Nginx',
+        objectivec   : 'Objective-C',
+        php          : 'PHP',
+        perl         : 'Perl',
+        python       : 'Python',
+        ruby         : 'Ruby',
+        scala        : 'Scala',
+        scss         : 'SCSS',
+        sql          : 'SQL',
+        swift        : 'Swift',
+        vbscript     : 'VBScript',
+        xml          : 'XML'
+    };
 
+    config.image_previewText = "JEESNS是一款基于JAVA企业级平台研发的社交管理系统，依托企业级JAVA的高效、安全、稳定等优势，开创国内JAVA版开源SNS先河，JEESNS可以用来搭建门户、论坛、社区、微博、知识付费平台等。";
     // 图片上传配置
     config.filebrowserUploadUrl = basePath + '/ckeditorUpload/uploadImage';
     config.filebrowserImageUploadUrl = basePath + '/ckeditorUpload/uploadImage';
